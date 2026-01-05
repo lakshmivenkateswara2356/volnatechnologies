04f7215f13ec4e9d2f2627bad713a34dbb3fb445
venkateswara8278 <venkateswarapunnam@clikkle.com>
2026-01-05 14:24:02 +0530

    changesofupdatedseo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import ScrollStack from "./components/ScrollStack";
+import SEO from "./components/SEO";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -10,6 +11,14 @@ import Contact from "./pages/Contact";
 const App = () => {
   return (
     <div className="relative">
+      {/* 🔥 SEO GOES HERE (TOP LEVEL) */}
+      <SEO
+        title="Volna | Web Development & Digital Solutions Company"
+        description="Volna is a professional web development company offering modern UI/UX design, frontend development, and scalable digital solutions."
+        keywords="Volna, web development company, React development, UI UX design, frontend development"
+        url="https://www.volna.co.in/"
+      />
+
       <Navbar />
 
       <ScrollStack
